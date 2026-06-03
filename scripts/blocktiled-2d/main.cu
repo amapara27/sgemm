e3098acc04d88b6aa20f50c0b49f3a2d1c9e7db2
amapara27 <amapara2007@gmail.com>
2026-06-02 18:49:28 -0700

    updated 2d blocktiling kernel

--- scripts/blocktiled-2d/main.cu
+++ scripts/blocktiled-2d/main.cu
@@ -5,33 +5,32 @@
 #include <iostream>
 
 #define CEIL_DIV(A, B) (((A) + (B) - 1) / (B))
-#define BLOCK_SIZE 32
 
 __global__ void sgemm_2d_bt(const float *a, const float *b, float *c, int K, int M, int N, float alpha, float beta) {
     // block tile dims - increase to 128 to improve occupancy: 64 - 256 threads
     const int bm = 128;
     const int bn = 128;
-    const int bk = 8; // chunks are 8 elements wide for a, 8 elements long for b
+    const int bk = 8; // 128 x 8 tile for a, 8 x 128 tile for b
 
     // each thread calculates tm * tn elements
     const int tm = 8;
     const int tn = 8;
 
-    // c matrix locations
+    // locations for the 128 x 128 tile the block is calculating
     int cRow = blockIdx.y;
     int cCol = blockIdx.x;
 
     // total results per blocktile and threads per block
-    int totalPerBT = bm * bn; // each block tile is bm x bn
-    int threadsPerBT = totalPerBT / (tm * tn); // total / amt per thread
+    int totalPerBT = bm * bn; // each block tile is bm x bn: 128 x 128 
+    int threadsPerBT = totalPerBT / (tm * tn); // total / amt per thread: 128 x 128 / 64 = 256 threads per block
 
     // ensure dimensions align: total results per blocktile / results per thread = threads per blocktile
     assert(threadsPerBT == blockDim.x); 
 
-    // anchor our threads - find the 8x8 batch they calculate
+    // anchor our threads - find the specific 8x8 batch they calculate within the block
     // bn / tn are number of threads that span a column
-    int tCol = threadIdx.x % (bn / tn); // cols: 0 - 15
-    int tRow = threadIdx.x / (bn / tn); // rows 0 - 15
+    int tCol = threadIdx.x % (bn / tn); // cols: 0 - 15 (16 cols x 8 elements per thread = 256 threads per block)
+    int tRow = threadIdx.x / (bn / tn); // rows 0 - 15 (16 rows x 8 elements per thread = 256 threads per block)
 
     // allocate shared memory for blocktiles
     __shared__ float as[bm * bk];
@@ -56,7 +55,7 @@ __global__ void sgemm_2d_bt(const float *a, const float *b, float *c, int K, int
     // allocate array for thread results (multiple entries of size tm * tn)
     float res[tm * tn] = {0.0f};
 
-    // number of iterations per thread
+    // number of iterations per thread: bk wide for matrix a, bk long for matrix b
     int num_bt = (K + bk - 1) / bk;
 
     // register caches for a and b
@@ -64,31 +63,31 @@ __global__ void sgemm_2d_bt(const float *a, const float *b, float *c, int K, int
     float regN[tn] = {0.0f};
 
     // iterate over block tiles
-    for (int i = 0; i < num_bt; i++) {
+    for (int bIdx = 0; bIdx < num_bt; bIdx++) {
         // populate SMEM caches for a
         for (int l = 0; l < bm; l += strideA) {
-            as[(iRowA + l) * bk + iColA] = a[(aRow + l) * K + i * bk + iColA]; // slide to the right for matrix a
+            as[(iRowA + l) * bk + iColA] = a[(aRow + l) * K + bIdx * bk + iColA]; // slide to the right for matrix a
         }
         
         // populate SMEM caches for b
         for (int l = 0; l < bk; l += strideB) {
-            bs[(iRowB + l) * bn + iColB] = b[(i * bk + l) * N + iRowB * N + bCol]; // slide down for matrix b
+            bs[(iRowB + l) * bn + iColB] = b[(bIdx * bk + l) * N + iRowB * N + bCol]; // slide down for matrix b
         }
 
         __syncthreads();
 
         // calculate per-thread result
-        for (int j = 0; j < bk; j++) {
+        for (int dotIdx = 0; dotIdx < bk; dotIdx++) {
             // block into registers
-            for (int m = 0; m < tm; m++) {
-                regM[m] = as[(tRow * tm + m) * bk + j];
+            for (int i = 0; i < tm; i++) {
+                regM[i] = as[(tRow * tm + i) * bk + dotIdx]; // 8 rows, after every dotIdx slide a col
             }
 
-            for (int m = 0; m < tm; m++) {
-                regN[m] = bs[(j * bn) + tCol * tn + m];
+            for (int i = 0; i < tn; i++) {
+                regN[i] = bs[(dotIdx * bn) + tCol * tn + i]; // 8 cols, after every dotIdx slide a row
             }
 
-            // iterate over a elements tm times (tm elements per thread)
+            // tm * tn elements
             for (int resIdxM = 0; resIdxM < tm; resIdxM++) {
                 for (int resIdxN = 0; resIdxN < tn; resIdxN++) {
                     res[resIdxM * tn + resIdxN] += regM[resIdxM] * regN[resIdxN];
